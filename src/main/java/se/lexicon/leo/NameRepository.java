e4e2cb345c91beb9cd2a0ae318c35fd734ac8493
leoodindealcantara <leoalcantara@icloud.com>
2021-06-08 14:49:22 +0200

    Assignment Arrays

--- src/main/java/se/lexicon/leo/NameRepository.java
+++ src/main/java/se/lexicon/leo/NameRepository.java
@@ -5,50 +5,56 @@ import java.util.Arrays;
 public class NameRepository {
 
     //Method 01
-    public static int getSize(String[] names) {
-        int size = names.length;
-        System.out.println("The size of the array is: " + size);
-        return size;
-    }
+    //public static int getSize(String[] names) {
+    //    int size = names.length;
+    //    System.out.println("The size of the array is: " + size);
+    //    return size;
+    //}
 
     //Method 02
-    public static void clear(String[] input) {
-        //int j = 0;
-        //for (int i = 0; i < string.length; i++) {
-         //   for (j = 0; j < string.length; j++) {
-           //     if (i == j) {
-        // }
-        //  }
-        //}
-        input = Arrays.copyOf(input, input.length - input.length);
-        System.out.println(Arrays.toString(input));
-
-    }
+    //public static void clear() {
+    //
+    //    names = Arrays.copyOf(names, names.length - names.length);
+    //    System.out.println(Arrays.toString(names));
+    //
+    //}
 
     //Method 03
-    public static boolean add (final String[] fullName){
-        String nameToAdd = new String();
-        names = Arrays.copyOf(fullName, fullName.length + 1);
-        fullName[fullName.length -1] = nameToAdd;
+    //public static boolean add (final String fullName){
+    //        Arrays.binarySearch(names, fullName);
+    //        if(nameFound != fullName) {
+    //            names = Arrays.copyOf(names, names.length + 1);
+    //            names[names.length-1] = fullName;
+    //            //names = Arrays.copyOf(names, names.length);
+    //            System.out.println(Arrays.toString(names));
+    //         return true;
+    //        } else {
+    //            System.out.println("Name has already been added to array.");
+    //        }
+    //       return true;
+    //}
 
-        for(int i = 0; i < fullName.length; i++){
-            if(fullName[i]!= nameToAdd) {
-            } return true;
+   //Method 04
+    public static String find(final String fullName){
+        for (String nameFound : names) {
+            if (fullName.equals(nameFound)){
+                System.out.println(fullName);
+                return fullName;
+           } else{
+                System.out.println("Name not found.");
+             return null;
+            }
         }
-       return false;
+             return null;
     }
 
-    /*Method 04
-    public static String find(final String fullName){
-
-
-    }*/
-
-    /*Method 05
-    public static String[] findAll(){
-
-
-    }*/
+    //Method 05
+    //public static String[] findAll(){
+    //    String [] newNames= Arrays.copyOf(names, names.length);
+    //    System.out.print("Array newNames: ");
+    //    System.out.println(Arrays.toString(newNames));
+    //    return newNames;
+    //}
 
     /*Method 06
     public static boolean update(final String original, final String updatedName){
@@ -56,11 +62,16 @@ public class NameRepository {
 
     }*/
 
-    /*Method 07
-    public static String[] findByFirstName(final String firstName){
-
-
-    }*/
+    //Method 07
+    //public static String[] findByFirstName(final String firstName){
+    //    String[] matchedNames={};
+    //    matchedNames[matchedNames.length-1]=firstName;
+    //    names.contains(firstName);
+    //
+    //
+    //
+    //      return firstName;
+    //}
 
 
     /*Method 08
@@ -69,18 +80,23 @@ public class NameRepository {
 
     }*/
 
-    private static String[] names = {"Erik", " ", "Svensson"};
+    private static String[] names = {"Erik Svensson", "Simon Elbrink"};
 
 
     public static void main(String[] args) {
 
        // getSize(names);
         //clear(names);
-        String nameToAdd = "Alcantara";
-        add(names);
 
+       //String nameToAdd = "Simon Elbrink";
+       //add(nameToAdd);
 
+      String nameFound = "Simon Elbrink";
+      find(nameFound);
 
+      //findAll();
+
+       //String firstName= "";
 
 
     }
